5bb836786ac56bddf8966823daafc9db35556895
unknown <SUITE@SUITEBG210-ST12.cibertec.edu.pe>
2023-05-22 16:20:35 -0500

    Starting the exam

---
+++ src/main/java/com/cibertec/dawi_cl2_quispe_edson/DawiCl2QuispeEdsonApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.dawi_cl2_quispe_edson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl2QuispeEdsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl2QuispeEdsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/dawi_cl2_quispe_edson/DawiCl2QuispeEdsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.dawi_cl2_quispe_edson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl2QuispeEdsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
